90c9910b660fa6fcfb2ca0965870d1081b084732
AmeghM <ameghm407@gmail.com>
2025-10-04 19:21:30 +0530

    Fix routing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer className="bg-gray-200 px-4 md:px-16 lg:px-28 py-8">
+    <footer className="bg-gray-200 px-4 md:px-16 lg:px-28 py-8 mt-5">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
         <div>
           <h2 className="text-lg font-bold mb-4 text-[#059669]">
